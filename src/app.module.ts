524a9ac87efd547d68c16f14ae9920ab60bbc93f
Julio Hernandez <jahp8719@gmail.com>
2022-11-03 09:44:22 -0400

    feact: initial commit

---
+++ src/app.module.ts
@@ -0,0 +1,31 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { config } from './config';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: `environment/${process.env.NODE_ENV}.env`,
+      load: [config],
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: () => ({
+        type: 'postgres',
+        host: process.env.DB_HOST,
+        port: Number(process.env.DB_PORT),
+        username: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_DATABASE,
+        entities: ['dist/**/*.entity{.ts,.js}'],
+        synchronize: true,
+      }),
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
